435ab33d7913f0eebfc4e9377cb4330cccecba03
Daniel Silva <danielsilva@iMac-de-Daniel.local>
2021-08-13 12:15:18 +0100

    features: fizzbuzz & fibonacci stuff done

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require("funcs.php");
+
+
+/* Exercise FizzBuzz */
+{
+run_fizz();
+}
+/* ---------------- */
+
+echo("<hr>");
+
+/* Exercise Fibonacci */
+{
+$val = 5;
+echo run_fib($val);
+}
+/* ---------------- */
+
+?>
